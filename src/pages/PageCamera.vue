7c95a02d5e19add07e1857ceb2a389f6f4f194e1
Ana Luiza <analu@Anas-MacBook-Pro.local>
2020-09-18 16:39:34 -0300

    fixed location not being found

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,12 +1,7 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video
-        v-show="!imageCaptured"
-        ref="video"
-        class="full-width"
-        autoplay
-      />
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
       <canvas
         v-show="imageCaptured"
         ref="canvas"
@@ -79,17 +74,17 @@
 </template>
 
 <script>
-import { uid } from 'quasar'
-require('md-gum-polyfill')
+import { uid } from "quasar";
+require("md-gum-polyfill");
 
 export default {
-  name: 'PageCamera',
-  data () {
+  name: "PageCamera",
+  data() {
     return {
       post: {
         id: uid(),
-        caption: '',
-        location: '',
+        caption: "",
+        location: "",
         photo: null,
         date: Date.now()
       },
@@ -97,66 +92,72 @@ export default {
       imageUpload: [],
       hasCameraSupport: true,
       locationLoading: false
-    }
+    };
   },
   computed: {
-    locationSupported () {
-      if ('geolocation' in navigator) return true
-      return false
+    locationSupported() {
+      if ("geolocation" in navigator) return true;
+      return false;
     }
   },
   methods: {
-    initCamera () {
-      navigator.mediaDevices.getUserMedia({
-        video: true
-      }).then(stream => {
-        this.$refs.video.srcObject = stream
-      }).catch(error => {
-        this.hasCameraSupport = false
-      })
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true
+        })
+        .then(stream => {
+          this.$refs.video.srcObject = stream;
+        })
+        .catch(error => {
+          this.hasCameraSupport = false;
+        });
     },
-    captureImage () {
-      let video = this.$refs.video
-      let canvas = this.$refs.canvas
-      canvas.width = video.getBoundingClientRect().width
-      canvas.height = video.getBoundingClientRect().height
-      let context = canvas.getContext('2d')
-      context.drawImage(video, 0, 0, canvas.width, canvas.height)
-      this.imageCaptured = true
-      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
-      this.disableCamera()
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
     },
-    captureImageFallback (file) {
-      this.post.photo = file
+    captureImageFallback(file) {
+      this.post.photo = file;
 
-      let canvas = this.$refs.canvas
-      let context = canvas.getContext('2d')
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext("2d");
 
-      var reader = new FileReader()
+      var reader = new FileReader();
       reader.onload = event => {
-        var img = new Image()
+        var img = new Image();
         img.onload = () => {
-          canvas.width = img.width
-          canvas.height = img.height
-          context.drawImage(img, 0, 0)
-          this.imageCaptured = true
-        }
-        img.src = event.target.result
-      }
-      reader.readAsDataURL(file)
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        };
+        img.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
     },
-    disableCamera () {
+    disableCamera() {
       this.$refs.video.srcObject.getVideoTracks().forEach(track => {
-        track.stop()
-      })
+        track.stop();
+      });
     },
-    dataURItoBlob (dataURI) {
+    dataURItoBlob(dataURI) {
       // convert base64 to raw binary data held in a string
       // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-      var byteString = atob(dataURI.split(',')[1]);
+      var byteString = atob(dataURI.split(",")[1]);
 
       // separate out the mime component
-      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      var mimeString = dataURI
+        .split(",")[0]
+        .split(":")[1]
+        .split(";")[0];
 
       // write the bytes of the string to an ArrayBuffer
       var ab = new ArrayBuffer(byteString.length);
@@ -172,77 +173,84 @@ export default {
       // write the ArrayBuffer to a blob, and you're done
       var blob = new Blob([ab], { type: mimeString });
       return blob;
-
     },
-    getLocation () {
-      this.locationLoading = true
-      navigator.geolocation.getCurrentPosition(position => {
-        this.getCityAndCountry(position)
-      }, err => {
-        this.locationError()
-      }, { timeout: 7000 })
+    getLocation() {
+      this.locationLoading = true;
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.getCityAndCountry(position);
+        },
+        err => {
+          this.locationError();
+        },
+        { timeout: 7000 }
+      );
     },
-    getCityAndCountry (position) {
-      let apiUrl = `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1`
-      this.$axios.get(apiUrl).then(result => {
-        this.locationSuccess(result)
-      }).catch(err => {
-        this.locationError()
-      })
+    getCityAndCountry(position) {
+      let apiUrl = `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1`;
+      this.$axios
+        .get(apiUrl)
+        .then(result => {
+          this.locationSuccess(result);
+        })
+        .catch(err => {
+          this.locationError();
+        });
     },
-    locationSuccess (result) {
-      this.post.location = result.data.city
+    locationSuccess(result) {
+      this.post.location = result.data.city;
       if (result.data.country) {
-        this.post.location += `, ${result.data.country}`
+        this.post.location += `, ${result.data.country}`;
       }
-      this.locationLoading = false
+      this.locationLoading = false;
     },
-    locationError () {
+    locationError() {
       this.$q.dialog({
-        title: 'Error',
-        message: 'Could not find your location.'
-      })
-      this.locationLoading = false
+        title: "Error",
+        message: "Could not find your location."
+      });
+      this.locationLoading = false;
     },
-    addPost () {
-      this.$q.loading.show()
+    addPost() {
+      this.$q.loading.show();
 
-      let formData = new FormData()
-      formData.append('id', this.post.id)
-      formData.append('caption', this.post.caption)
-      formData.append('location', this.post.location)
-      formData.append('date', this.post.date)
-      formData.append('file', this.post.photo, this.post.id + '.png')
+      let formData = new FormData();
+      formData.append("id", this.post.id);
+      formData.append("caption", this.post.caption);
+      formData.append("location", this.post.location);
+      formData.append("date", this.post.date);
+      formData.append("file", this.post.photo, this.post.id + ".png");
 
-      this.$axios.post(`${process.env.API}/createPost`, formData).then(response => {
-        console.log('response: ', response)
-        this.$router.push('/')
-        this.$q.notify({
-          message: 'Post created!',
-          actions: [
-            { label: 'Dismiss', color: 'white' }
-          ]
+      this.$axios
+        .post(`${process.env.API}/createPost`, formData)
+        .then(response => {
+          console.log("response: ", response);
+          this.$router.push("/");
+          this.$q.notify({
+            message: "Post created!",
+            actions: [{ label: "Dismiss", color: "white" }]
+          });
+          this.$q.loading.hide();
         })
-        this.$q.loading.hide()
-      }).catch(err => {
-        console.log('err: ', err)
-        this.$q.dialog({
-          title: 'Error',
-          message: 'Sorry, could not create post!'
-        })
-        this.$q.loading.hide()
-      })
+        .catch(err => {
+          console.log("err: ", err);
+          this.$q.dialog({
+            title: "Error",
+            message: "Sorry, could not create post!"
+          });
+          this.$q.loading.hide();
+        });
     }
   },
-  mounted () {
-    this.initCamera()
+  mounted() {
+    this.initCamera();
   },
-  beforeDestroy () {
+  beforeDestroy() {
     if (this.hasCameraSupport) {
-      this.disableCamera()
+      this.disableCamera();
     }
   }
-}
+};
 </script>
 
 <style lang="sass">
